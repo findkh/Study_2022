5b66340b42a3465f561737262c31c55e0ba6e3e8
KyoungHyunPark <findkh@nate.com>
2022-05-06 15:01:39 +0900

    220506

---
+++ src/Baekjoon/IoAndCalculation/sprout.java
@@ -0,0 +1,21 @@
+/*
+문제
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+ */
+
+package Baekjoon.IoAndCalculation;
+public class sprout {
+  public static void main(String[] args) {
+    System.out.println("         ,r'\"7");
+    System.out.println("r`-_   ,'  ,/");
+    System.out.println(" \\. \". L_r'");
+    System.out.println("   `~\\/");
+    System.out.println("      |");
+    System.out.println("      |");
+  }
+}
