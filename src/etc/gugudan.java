e065f841b00e57fc2f0fc76e2220d03e1d5810d1
KyoungHyunPark <findkh@nate.com>
2022-05-22 20:15:45 +0900

    .

---
+++ src/etc/gugudan.java
@@ -0,0 +1,11 @@
+package etc;
+
+public class gugudan {
+  public static void main(String[] args) {
+    for (int a = 2; a <= 9; a++) {
+      for (int b = 1; b <= 9; b++) {
+        System.out.printf("%d * %d = %d\n", a, b, a*b);
+      }
+    }
+  }
+}
