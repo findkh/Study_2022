4660899dcb4393dadaae35cacecd3423bf6135a5
findk <findkh@nate.com>
2022-05-29 19:48:42 +0900

    비즈니스 계층

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.zerock.domain.BoardVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -17,10 +16,10 @@ public class BoardMapperTests {
   @Setter(onMethod_ = @Autowired)
   private BoardMapper mapper;
 
-  //  @Test
-  //  public void testGetList() {
-  //    mapper.getList().forEach(board -> log.info(board));
-  //  }
+  @Test
+  public void testGetList() {
+    mapper.getList().forEach(board -> log.info(board));
+  }
 
   //  @Test
   //  public void testInsert() {
@@ -57,17 +56,16 @@ public class BoardMapperTests {
   //    log.info("DELETE COUNT: " + mapper.delete(3L));
   //  }
 
-  @Test
-  public void testUpdate() {
-    BoardVO board = new BoardVO();
-
-    board.setBno(5L);
-    board.setTitle("수정 제목");
-    board.setContent("수정 내용");
-    board.setWriter("user00");
-
-    int count = mapper.update(board);
-    log.info("UPDATE COUNT: " + count);
-  }
-
+  //  @Test
+  //  public void testUpdate() {
+  //    BoardVO board = new BoardVO();
+  //
+  //    board.setBno(5L);
+  //    board.setTitle("수정 제목");
+  //    board.setContent("수정 내용");
+  //    board.setWriter("user00");
+  //
+  //    int count = mapper.update(board);
+  //    log.info("UPDATE COUNT: " + count);
+  //  }
 }
