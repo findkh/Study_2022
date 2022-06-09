fcc82ada0cceb7bee0ca52dc47b44fb8998563cd
KyoungHyunPark <findkh@nate.com>
2022-06-09 20:13:34 +0900

    문자열 비교

---
+++ src/etc/operator.java
@@ -0,0 +1,18 @@
+package etc;
+
+public class operator {
+  //문자열 비교
+  public static void main(String[] args) {
+    String str1 = "abc";
+    String str2 = new String("abc");
+    String[] strArr = {"abc", "ABC"};
+    char charI = 'i';
+    System.out.println(strArr[0] == str2);
+    System.out.println(strArr[0].equals(str1));
+    System.out.println(strArr[1].equals(str1));
+    System.out.println(strArr[1].equalsIgnoreCase(str1));
+    System.out.println(strArr[0] == "ABC");
+
+    System.out.println(charI == 'i');
+  }
+}
