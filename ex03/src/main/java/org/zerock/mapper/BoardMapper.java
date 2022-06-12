0cf7ad29b63b04870f6360fe47e760282d0333cd
findk <findkh@nate.com>
2022-06-12 21:42:42 +0900

    댓글 수 처리

--- ex03/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex03/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -1,6 +1,7 @@
 package org.zerock.mapper;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 
@@ -11,8 +12,6 @@ public interface BoardMapper {
 
   public List<BoardVO> getListWithPaging(Criteria cri);
 
-  public int getTotalCount(Criteria cri);
-
   public void insert(BoardVO board);
 
   public void insertSelectKey(BoardVO board);
@@ -22,5 +21,9 @@ public interface BoardMapper {
   public int delete(Long bno);
 
   public int update(BoardVO board);
+
+  public int getTotalCount(Criteria cri);
+
+  public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
 
 }
--- ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -3,9 +3,11 @@ package org.zerock.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyPageDTO;
 import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.BoardMapper;
 import org.zerock.mapper.ReplyMapper;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
@@ -20,6 +22,10 @@ public class ReplyServiceImpl implements ReplyService{
   @Setter(onMethod_ = @Autowired)
   private ReplyMapper mapper;
 
+  @Setter(onMethod_ = @Autowired)
+  private BoardMapper boardMapper;
+
+  @Transactional
   @Override
   public int register(ReplyVO vo) {
     log.info("register..." + vo);
@@ -38,6 +44,7 @@ public class ReplyServiceImpl implements ReplyService{
     return mapper.update(vo);
   }
 
+  @Transactional
   @Override
   public int remove(Long rno) {
     log.info("remove..." + rno);
