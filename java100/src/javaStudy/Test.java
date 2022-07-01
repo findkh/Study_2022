88768859a68e7a5c2c36d04a018f666b1249e96f
findk <findkh@nate.com>
2022-07-01 20:18:50 +0900

    .

--- java100/src/javaStudy/Test.java
+++ java100/src/javaStudy/Test.java
@@ -1,7 +1,7 @@
 package javaStudy;
 
-public class study01 {
+public class Test {
   public static void main(String[] args) {
-    System.out.println("Hello, world!"); 
+    System.out.println("Hello");
   }
 }
