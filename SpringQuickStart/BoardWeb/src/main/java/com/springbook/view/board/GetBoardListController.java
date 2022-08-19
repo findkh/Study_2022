a9223f3a8e205f69d17d9ead703b5a53136c6f90
findk <findkh@nate.com>
2022-08-19 22:25:23 +0900

    어노테이션 기반 MVC 개발

--- SpringQuickStart/BoardWeb/src/main/java/com/springbook/view/board/GetBoardListController.java
+++ SpringQuickStart/BoardWeb/src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -1,31 +1,34 @@
 package com.springbook.view.board;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
-public class GetBoardListController implements Controller {
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+//@Controller
+public class GetBoardListController {
+	
+	//@RequestMapping("/getBoardList.do")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+	
 		System.out.println("글 목록 검색 처리");
 		
-		//1. 사용자 입력 정보 추출
-		//2. DB 연동 처리
-		BoardVO vo = new BoardVO();
-		BoardDAO boardDAO = new BoardDAO();
-		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+//		//1. 사용자 입력 정보 추출
+//		//2. DB 연동 처리
+//		BoardVO vo = new BoardVO();
+//		BoardDAO boardDAO = new BoardDAO();
+//		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+//		
+//		// 3. 검색 결과와 화면 정보를 MadelAndView에 저장하여 리턴한다.
+//		ModelAndView mav = new ModelAndView();
+//		mav.addObject("boardList", boardList);
+//		mav.setViewName("getBoardList");
+//		return mav;
 		
-		// 3. 검색 결과와 화면 정보를 MadelAndView에 저장하여 리턴한다.
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("boardList", boardList);
-		mav.setViewName("getBoardList");
+		mav.addObject("boardList", boardDAO.getBoardList(vo)); //model 정보 저장
+		mav.setViewName("getBoardList.jsp"); //view 정보 저장
 		return mav;
 	}
 }
