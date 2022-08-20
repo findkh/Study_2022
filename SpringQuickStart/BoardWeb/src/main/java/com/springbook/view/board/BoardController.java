3901ab9896e43da3ce5b61a383b141b8988b3bda
findk <findkh@nate.com>
2022-08-20 11:15:17 +0900

    어노테이션으로 구현

--- SpringQuickStart/BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ SpringQuickStart/BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,13 +1,19 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
 	
 	@RequestMapping(value="/insertBoard.do")
@@ -18,7 +24,15 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+		
+		System.out.println("번호 : " + vo.getSeq());
+		System.out.println("제목 : " + vo.getTitle());
+		System.out.println("작성자 : " + vo.getWriter());
+		System.out.println("내용 : " + vo.getContent());
+		System.out.println("등록일 : " + vo.getRegDate());
+		System.out.println("조회수 : " + vo.getCnt());
+		
 		System.out.println("글 수정 처리");
 		boardDAO.updateBoard(vo);
 		return "getBoardList.do";
@@ -32,19 +46,36 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/getBoard.do")
-	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
 		System.out.println("글 상세 조회 처리");
-		mav.addObject("board", boardDAO.getBoard(vo));
-		mav.setViewName("getBoard.jsp");
-		return mav;		
+		model.addAttribute("board", boardDAO.getBoard(vo)); //모델 정보 저장
+		return "getBoard.jsp";
+	}
+	
+//	@RequestMapping("/getBoardList.do")
+//	public String getBoardList(@RequestParam(value="searchCondition", defaultValue="TITLE", required=false) String condition, 
+//		@RequestParam(value="searchKeyword", defaultValue="", required=false)String keyword, BoardVO vo, BoardDAO boardDAO, Model model) {	
+//		System.out.println("글 목록 검색 처리");
+//		System.out.println("검색 조건: " + condition);
+//		System.out.println("검색 단어: " + keyword);
+//		model.addAttribute("boardList", boardDAO.getBoardList(vo)); //model 정보 저장
+//		return "getBoardList.jsp"; //뷰 이름 리턴
+//	}
+
+	//검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+			Map<String, String> conditionMap = new HashMap<String, String>();
+			conditionMap.put("제목", "TITLE");
+			conditionMap.put("내용", "CONTENT");
+			return conditionMap;
 	}
 	
 	@RequestMapping("/getBoardList.do")
-	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {	
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {	
 		System.out.println("글 목록 검색 처리");
-		mav.addObject("boardList", boardDAO.getBoardList(vo)); //model 정보 저장
-		mav.setViewName("getBoardList.jsp"); //view 정보 저장
-		return mav;
+		model.addAttribute("boardList", boardDAO.getBoardList(vo)); //model 정보 저장
+		return "getBoardList.jsp"; //뷰 이름 리턴
 	}
 	
 }
--- SpringQuickStart/BoardWeb/src/main/java/com/springbook/view/user/LoginController.java
+++ SpringQuickStart/BoardWeb/src/main/java/com/springbook/view/user/LoginController.java
@@ -1,8 +1,10 @@
 package com.springbook.view.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
@@ -10,36 +12,55 @@ import com.springbook.biz.user.impl.UserDAO;
 @Controller
 public class LoginController{
 	
-	@RequestMapping("/login.do")
-	public String login(UserVO vo, UserDAO userDAO) {
-		System.out.println("로그인 처리");
-		
-//		//1. 사용자 입력 정보 추출
-//		String id = request.getParameter("id");
-//		String password = request.getParameter("password");
+//	@RequestMapping("/login.do")
+//	public String login(UserVO vo, UserDAO userDAO) {
+//		System.out.println("로그인 처리");
 //		
-//		//2. DB 연동 처리
-//		UserVO vo = new UserVO();
-//		vo.setId(id);
-//		vo.setPassword(password);
+////		//1. 사용자 입력 정보 추출
+////		String id = request.getParameter("id");
+////		String password = request.getParameter("password");
+////		
+////		//2. DB 연동 처리
+////		UserVO vo = new UserVO();
+////		vo.setId(id);
+////		vo.setPassword(password);
+////		
+////		UserDAO userDAO = new UserDAO();
+////		UserVO user = userDAO.getUser(vo);
+////		
+////		//3. 화면 네비게이션
+////		ModelAndView mav = new ModelAndView();
+////		if(user != null) {
+////			mav.setViewName("redirect:getBoardList.do");
+////		} else {
+////			mav.setViewName("redirect:login.jsp");
+////		}
+////		return mav;
 //		
-//		UserDAO userDAO = new UserDAO();
-//		UserVO user = userDAO.getUser(vo);
-//		
-//		//3. 화면 네비게이션
-//		ModelAndView mav = new ModelAndView();
-//		if(user != null) {
-//			mav.setViewName("redirect:getBoardList.do");
+//		if(userDAO.getUser(vo) != null) {
+//			return "getBoardList.do";
 //		} else {
-//			mav.setViewName("redirect:login.jsp");
+//			return "login.jsp";
 //		}
-//		return mav;
-		
-		if(userDAO.getUser(vo) != null) {
+	
+	//메서드 이용
+	@RequestMapping(value="/login.do", method=RequestMethod.GET)
+	public String loginView(UserVO vo) {
+		System.out.println("로그인 화면으로 이동");
+		vo.setId("test");
+		vo.setPassword("test123");
+		return "login.jsp";		
+	}
+	
+	@RequestMapping(value="/login.do", method=RequestMethod.POST)
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+		System.out.println("로그인 인증 처리");
+		UserVO user = userDAO.getUser(vo);
+		if(user != null) {
+			session.setAttribute("userName", user.getName());
 			return "getBoardList.do";
 		} else {
 			return "login.jsp";
 		}
-		
 	}
 }
