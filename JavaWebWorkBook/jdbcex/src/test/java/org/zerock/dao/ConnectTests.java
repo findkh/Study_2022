58ae22f32a90acff4827ce77757d2a8420e50920
findk <findkh@nate.com>
2022-09-10 21:43:56 +0900

    자바웹개발워크북 - 프로젝트 내 JDBC 구현

--- JavaWebWorkBook/jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
+++ JavaWebWorkBook/jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -1,5 +1,7 @@
 package org.zerock.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +10,13 @@ import java.sql.DriverManager;
 
 public class ConnectTests {
 
-//    @Test
-//    public void test1() {
-//        int v1 = 10;
-//        int v2 = 10;
-//
-//        Assertions.assertEquals(v1, v2);
-//    }
+    @Test
+    public void test1() {
+        int v1 = 10;
+        int v2 = 10;
+
+        Assertions.assertEquals(v1, v2);
+    }
 
     @Test
     public void testConnection() throws Exception {
@@ -26,6 +28,25 @@ public class ConnectTests {
                 "webuser");
 
         Assertions.assertNotNull(connection);
+
+        connection.close();
+    }
+
+    @Test
+    public void testHikariCP() throws Exception {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("webuser");
+        config.addDataSourceProperty("cachePrepStmts", true);
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
 
         connection.close();
     }
